1f6570675d4aa336ca6a6574c35e69d54536ee33
Daniel-Thompson-91 <danielthompson.cc@gmail.com>
2022-09-06 17:56:22 -0500

    edit main.js

--- resources/script/main.js
+++ resources/script/main.js
@@ -1,4 +1,4 @@
-//retrieve 3 year reading plan data
+//retrieve 3 year reading plan data from json file
 const weeklyReadings = require('../../json/readingPlan.json');
 
 //get date info
@@ -7,11 +7,10 @@ const currentWeekDay = currentDate.getDay();
 const currentMonth = currentDate.getMonth();
 const currentMonthDay = currentDate.getDate();
 
+//reading plan date info
+const readingPlanStartDate = new Date('March 22, 2020 6:00');
+const currentReadingWeek = 0;
 
 const reading1 = document.getElementById('reading1');
 const reading2 = document.getElementById('reading2');
 const reading3 = document.getElementById('reading3');
-
-const setWeek = (week) => {
-  
-};
