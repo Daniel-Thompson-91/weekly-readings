27f82b4398dbffbb1162d8e8a7a2890b180b094a
Daniel-Thompson-91 <danielthompson.cc@gmail.com>
2022-09-07 18:43:52 -0500

    Update main.mjs

--- resources/scripts/main.mjs
+++ resources/scripts/main.mjs
@@ -1,16 +1,23 @@
 //retrieve 3 year reading plan data from json file
 import {weeklyReadings} from './readingPlan.mjs';
-console.log(weeklyReadings);
+
 //get date info
 const startDate = new Date('March 28, 2020');
 const currentDate = new Date();
 const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
 const currentWeek = Math.ceil(days / 7);
 
-/*const reading1InJson = weeklyReadings.reading1;
-const reading2InJson = weeklyReadings.reading2;
-const reading3InJson = weeklyReadings.reading3;
+//retrieve DOM elements to be changed dynamically
+const reading1 = document.getElementById('reading1');
+const reading2 = document.getElementById('reading2');
+const reading3 = document.getElementById('reading3');
 
-const reading1InApp = document.getElementById('reading1');
-const reading2InApp = document.getElementById('reading2');
-const reading3InApp = document.getElementById('reading3');*/
+const setReading1 = () => {
+  for (let i = 0; i < 143; i++) {
+    if (weeklyReadings.weekNumber[i] === currentWeek) {
+      reading1.innerHTML = weeklyReadings.reading1[i];
+    }
+  }
+}
+
+console.log(setReading1);
