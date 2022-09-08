630cfb0a9ca640970dd8ae394066b7958cf9b2fc
Daniel-Thompson-91 <danielthompson.cc@gmail.com>
2022-09-08 10:18:32 -0500

    Update main.mjs
    try using fs to read and parse json file for changing reading# text

--- resources/scripts/main.mjs
+++ resources/scripts/main.mjs
@@ -1,5 +1,6 @@
-//retrieve 3 year reading plan data from json file
-import {weeklyReadings} from './readingPlan.mjs';
+const fs = require('fs');
+
+let readingPlan;
 
 //get date info
 const startDate = new Date('March 28, 2020');
@@ -12,12 +13,16 @@ const reading1 = document.getElementById('reading1');
 const reading2 = document.getElementById('reading2');
 const reading3 = document.getElementById('reading3');
 
-const setReading1 = () => {
-  for (let i = 0; i < 143; i++) {
-    if (weeklyReadings.weekNumber[i] === currentWeek) {
-      reading1.innerHTML = weeklyReadings.reading1[i];
+//read json and set reading1, reading2, and reading3 innerHTML
+fs.readFile('https://github.com/Daniel-Thompson-91/weekly-readings/blob/main/json/readingPlan.json', 'utf8', (err, jsonData) => {
+  if (err) throw err;
+  readingPlan = JSON.parse(jsonData);
+
+  for (let i = 0; i < readingPlan.length; i++) {
+    if (readingPlan[i].weekNumber === currentWeek) {
+      reading1.innerHTML = readingPlan[i].reading1;
+      reading2.innerHTML = readingPlan[i].reading2;
+      reading3.innerHTML = readingPlan[i].reading3;
     }
   }
-}
-
-console.log(setReading1);
+})
