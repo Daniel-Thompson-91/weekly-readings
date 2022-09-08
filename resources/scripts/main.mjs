b3953379031435d4e86dec115e562d4082fdb5aa
Daniel-Thompson-91 <danielthompson.cc@gmail.com>
2022-09-08 11:19:45 -0500

    made stupid changes again because I don't know what the heck I'm doing!

--- resources/scripts/main.mjs
+++ resources/scripts/main.mjs
@@ -1,7 +1,3 @@
-const fs = require('fs');
-
-let readingPlan;
-
 //get date info
 const startDate = new Date('March 28, 2020');
 const currentDate = new Date();
@@ -12,17 +8,3 @@ const currentWeek = Math.ceil(days / 7);
 const reading1 = document.getElementById('reading1');
 const reading2 = document.getElementById('reading2');
 const reading3 = document.getElementById('reading3');
-
-//read json and set reading1, reading2, and reading3 innerHTML
-fs.readFile('https://github.com/Daniel-Thompson-91/weekly-readings/blob/main/json/readingPlan.json', 'utf8', (err, jsonData) => {
-  if (err) throw err;
-  readingPlan = JSON.parse(jsonData);
-
-  for (let i = 0; i < readingPlan.length; i++) {
-    if (readingPlan[i].weekNumber === currentWeek) {
-      reading1.innerHTML = readingPlan[i].reading1;
-      reading2.innerHTML = readingPlan[i].reading2;
-      reading3.innerHTML = readingPlan[i].reading3;
-    }
-  }
-})
